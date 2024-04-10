2e342d8db26f4fce6804ace4b31936dac4c829f7
Rosa V <rosalinde.vermeulen@gmail.com>
2024-04-10 16:02:43 +0200

    dark mode
    dark mode added and a bit of styling

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
 // time for some code
+
+function darkMode(){
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+    var changeText = document.getElementById("changeText");
+    if (changeText.innerHTML === "🌑") {
+        changeText.innerHTML = "☀️";
+      } else {
+        changeText.innerHTML = "🌑";
+      }
+}
+
