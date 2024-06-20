011081d8ed1a3e9fae8bf34b40f18db5da3425e3
Petrichorius <rosalinde.vermeulen@gmail.com>
2024-06-21 00:49:37 +0200

    update
    chapter select and working on title scroll

--- main.js
+++ main.js
@@ -14,3 +14,51 @@ function darkMode(){
 function scrollToElement(scrollTo){
   document.getElementById(scrollTo).scrollIntoView({behavior: 'smooth'});
 }
+
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+} 
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    const stickyText = document.querySelector('.sticky-text');
+    const chapters = document.querySelectorAll('.chapter');
+
+    const observerOptions = {
+        root: null,
+        rootMargin: '0px',
+        threshold: 0.1
+    };
+
+    const observerCallback = (entries, observer) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                stickyText.textContent = entry.target.querySelector('.stickyText').textContent;
+                stickyText.style.display = 'block';
+            } else if (!entry.isIntersecting && entry.boundingClientRect.top > 0) {
+                stickyText.style.display = 'none';
+            }
+        });
+    };
+
+    const observer = new IntersectionObserver(observerCallback, observerOptions);
+
+    chapters.forEach(chapter => {
+        observer.observe(chapter);
+    });
+});
